b1c5e48263fc0cfb3d6e23890e5a67e16a71a352
livos <lvivancos@gmail.com>
2016-12-22 10:26:25 +0700

    App ok

--- app/src/main/java/com/example/android/keystore/MainActivity.java
+++ app/src/main/java/com/example/android/keystore/MainActivity.java
@@ -1,13 +1,193 @@
 package com.example.android.keystore;
 
+import android.content.DialogInterface;
+import android.security.KeyPairGeneratorSpec;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.PrivateKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Enumeration;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.security.auth.x500.X500Principal;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "APP_LOG";
+    private final static String alias = "mon alias bidon";
+    private String textToEncrypt = "A la claire fontaine";
+    private String encryptedText = "";
+    private String decryptedText = "";
+    KeyStore keyStore;
+    ArrayList keyAliases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG,textToEncrypt);
+
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        refreshKeys();
+
+        createNewKeys();
+        encryptString();
+        decryptString();
+
+    }
+
+    private void refreshKeys() {
+        keyAliases = new ArrayList<>();
+        try {
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                keyAliases.add(aliases.nextElement());
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createNewKeys() {
+        try {
+            // Create new key if needed
+            if (!keyStore.containsAlias(alias)) {
+                Calendar start = Calendar.getInstance();
+                Calendar end = Calendar.getInstance();
+                end.add(Calendar.YEAR, 1);
+                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(this)
+                        .setAlias(alias)
+                        .setSubject(new X500Principal("CN=Sample Name, O=Android Authority"))
+                        .setSerialNumber(BigInteger.ONE)
+                        .setStartDate(start.getTime())
+                        .setEndDate(end.getTime())
+                        .build();
+                KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
+                generator.initialize(spec);
+
+                KeyPair keyPair = generator.generateKeyPair();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        refreshKeys();
+    }
+
+    public void deleteKey(final String alias) {
+        AlertDialog alertDialog =new AlertDialog.Builder(this)
+                .setTitle("Delete Key")
+                .setMessage("Do you want to delete the key \"" + alias + "\" from the keystore?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            keyStore.deleteEntry(alias);
+                            refreshKeys();
+                        } catch (KeyStoreException e) {
+                            Toast.makeText(MainActivity.this,
+                                    "Exception " + e.getMessage() + " occured",
+                                    Toast.LENGTH_LONG).show();
+                            Log.e(TAG, Log.getStackTraceString(e));
+                        }
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        alertDialog.show();
+    }
+
+    public void encryptString() {
+        try {
+            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry)keyStore.getEntry(alias, null);
+            RSAPublicKey publicKey = (RSAPublicKey) privateKeyEntry.getCertificate().getPublicKey();
+
+            // Encrypt the text
+            String initialText = textToEncrypt;
+            if(initialText.isEmpty()) {
+                Toast.makeText(this, "Enter text in the 'Initial Text' widget", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Cipher input = Cipher.getInstance("RSA/ECB/PKCS1Padding", "AndroidOpenSSL");
+            input.init(Cipher.ENCRYPT_MODE, publicKey);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            CipherOutputStream cipherOutputStream = new CipherOutputStream(
+                    outputStream, input);
+            cipherOutputStream.write(initialText.getBytes("UTF-8"));
+            cipherOutputStream.close();
+
+            byte [] vals = outputStream.toByteArray();
+            encryptedText = Base64.encodeToString(vals, Base64.DEFAULT);
+            Log.d(TAG,encryptedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void decryptString() {
+        try {
+            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry)keyStore.getEntry(alias, null);
+            PrivateKey privateKey =  privateKeyEntry.getPrivateKey();
+
+            Cipher output = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            output.init(Cipher.DECRYPT_MODE, privateKey);
+
+            String cipherText = encryptedText;
+            CipherInputStream cipherInputStream = new CipherInputStream(
+                    new ByteArrayInputStream(Base64.decode(cipherText, Base64.DEFAULT)), output);
+            ArrayList<Byte> values = new ArrayList<>();
+            int nextByte;
+            while ((nextByte = cipherInputStream.read()) != -1) {
+                values.add((byte)nextByte);
+            }
+
+            byte[] bytes = new byte[values.size()];
+            for(int i = 0; i < bytes.length; i++) {
+                bytes[i] = values.get(i).byteValue();
+            }
+
+            String finalText = new String(bytes, 0, bytes.length, "UTF-8");
+            decryptedText = finalText;
+            Log.d(TAG,decryptedText);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
